bceb34bb489b92bc5679777dd91e9495cd557511
Molly Pfefferkorn <mollypfeff@gmail.com>
2025-01-02 18:24:55 -0500

    assignment updates

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -22,7 +22,7 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(autoload_with=engine)
 
-# Save references to each table
+# Save references to each table (we know the table names based on the jupyter hawaii_climate_tracking notebook)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
@@ -33,7 +33,8 @@ session = Session(engine)
 # Flask Setup
 #################################################
 
-
+#the homepage for the website I created (defined in function home) is
+#localhost:5000
 
 
 #################################################
@@ -61,7 +62,7 @@ def precipitation():
     #saving the precipitation data as a dictionary, using date as the key and prcp as the value
     precipitation_dictionary = {date: prcp for date, prcp in last12months} 
     return jsonify(precipitation_dictionary) #turning the resulting dictionary into a json
-
+#the page should display a dictionary showing every date between 8/23/2016 and 8/23/2017 and the corresponding precipitation measurement for each date.
 
 
 
@@ -71,6 +72,7 @@ def stations():
     session.close()
     stationList = list(np.ravel(station_list)) #convert results into a list using numpy.ravel
     return jsonify(stationList) #jsonify the list
+#the page should display a list of the stations by name.
 
 
 @app.route("/api/v1.0/tobs")
@@ -83,12 +85,15 @@ def temps():
     session.close()
     temperatureRecords = list(np.ravel(temp_records)) #convert results into a list using numpy.ravel
     return jsonify(temperatureRecords) #jsonify the list
+#the page should display a list containing the dates and temperatures logged at the most active plant (USC00519281) on that date for the previous year of data. 
+#Each date in the list is followed by the temperature logged on that date 
+# i.e., if row 0 lists '2016-08-23' and row 1 lists '77.0', that means the temperature logged at the most active plant on 8/23/2016 was 77 degrees.
 
 
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
 def dateStats(start=None, end=None):
-    stats = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
+    stats = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)] #calculating the min, max, and avg using the respective func functions.
 
     if not end:
         startDate = dt.datetime.strptime(start,"%m%d%Y")
@@ -96,6 +101,7 @@ def dateStats(start=None, end=None):
         session.close()
         tempList = list(np.ravel(results))
         return jsonify(tempList)
+    #if no end date is given, the resulting list contains min, max and avg temperatures from the 'start' date up to the most recent date in the data set.
 
     else:
         startDate = dt.datetime.strptime(start,"%m%d%Y")
@@ -104,7 +110,9 @@ def dateStats(start=None, end=None):
         session.close()
         tempList = list(np.ravel(results))
         return jsonify(tempList)
+    #if an end date is given, the resulting list contains min, max, and avg temperatures from the 'start' date up to and including the 'end' date.
     
 #################################################
 if __name__ == '__main__':
     app.run(debug=True)
+#other half of the Flask program
